efa91dc848db7f8b2de2edf5e051959d46ffe047
0xsublime <91898730+0xsublime@users.noreply.github.com>
2022-11-20 17:27:49 +0100

    set up some test users

--- test/ImbuedMinterV3.t.sol
+++ test/ImbuedMinterV3.t.sol
@@ -9,12 +9,16 @@ contract MinterTest is Test {
     ImbuedNFT nft;
     ImbuedMintV3 public minter;
     address imbuedDeployer = 0x34EeE73e731fB2A428444e2b2957C36A9b145017;
+    address[] users = [address(0x1), address(0x2), address(0x3)];
 
     function setUp() public {
         minter = new ImbuedMintV3();
-        vm.prank(imbuedDeployer);
-        nft = new ImbuedNFT();
+        vm.prank(imbuedDeployer); nft = new ImbuedNFT();
+        vm.prank(imbuedDeployer); nft.setMintContract(address(minter));
         assertEq(address(minter.NFT()), address(nft));
+        for (uint i = 0; i < users.length; i++) {
+            vm.deal(users[i], 10 ether);
+        }
     }
  
     function testSuccess() public {
