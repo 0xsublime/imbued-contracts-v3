0efb7818a05c011561c04128252d75860ece9fe5
0xsublime <91898730+0xsublime@users.noreply.github.com>
2022-11-20 17:20:48 +0100

    deploy nft contract in test

--- test/ImbuedMinterV3.t.sol
+++ test/ImbuedMinterV3.t.sol
@@ -3,12 +3,18 @@ pragma solidity ^0.8.13;
  
 import "forge-std/Test.sol";
 import "../src/ImbuedMinterV3.sol";
+import "../src/deployed/ImbuedNFT.sol";
  
 contract MinterTest is Test {
+    ImbuedNFT nft;
     ImbuedMintV3 public minter;
- 
+    address imbuedDeployer = 0x34EeE73e731fB2A428444e2b2957C36A9b145017;
+
     function setUp() public {
         minter = new ImbuedMintV3();
+        vm.prank(imbuedDeployer);
+        nft = new ImbuedNFT();
+        assertEq(address(minter.NFT()), address(nft));
     }
  
     function testSuccess() public {
